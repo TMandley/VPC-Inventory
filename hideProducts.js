f4b65bae77d1abba1d81abf42138b4e0bb9c3ba6
TMandley <51174167+TMandley@users.noreply.github.com>
2021-12-09 15:29:32 -0700

    Add files via upload

---
+++ hideProducts.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    const productUsers = [2, 21, 30, 53];
+    let currUser = walkMeData.currentUser.id;
+    let myDeleteFunction = function (e) {
+      let idx = $(this).index();
+      return idx == 1 || idx == 2 || idx == 3 || idx == 4 || idx == 6;
+    };
+    if(productUsers.includes(currUser)){
+      console.log('hello');
+    }else{
+      console.log('nope');
+      if(window.location.pathname == '/products/'){
+        let addButton = document.getElementsByClassName('btn btn-large pull-right no-print btn-success ng-isolate-scope')[0];
+        addButton.style.display = 'none';
+        $('form table tr td').filter(myDeleteFunction).css("display", "none");
+        $('form table tr td div div').filter(function(e) {
+          let idx = $(this).index();
+          return idx == 1 || idx == 2;
+        }).css("display", "none");
+        $('form table thead tr th').filter(myDeleteFunction).css("display", "none");
+      }
+    }
+});
