53fe0c056e5bded9b295579d3868346bb536511b
TMandley <51174167+TMandley@users.noreply.github.com>
2021-12-13 15:41:52 -0700

    Update hideProducts.js

--- hideProducts.js
+++ hideProducts.js
@@ -5,11 +5,13 @@ $(document).ready(function () {
       let idx = $(this).index();
       return idx == 1 || idx == 2 || idx == 3 || idx == 4 || idx == 6;
     };
+    let thePageIAmOn = window.location.pathname;
+    let myRegExGuy = /view/;
     if(productUsers.includes(currUser)){
       console.log('hello');
     }else{
       console.log('nope');
-      if(window.location.pathname == '/products/'){
+      if(thePageIAmOn == '/products/' || thePageIAmOn == '/products'){
         let addButton = document.getElementsByClassName('btn btn-large pull-right no-print btn-success ng-isolate-scope')[0];
         addButton.style.display = 'none';
         $('form table tr td').filter(myDeleteFunction).css("display", "none");
@@ -18,6 +20,13 @@ $(document).ready(function () {
           return idx == 1 || idx == 2;
         }).css("display", "none");
         $('form table thead tr th').filter(myDeleteFunction).css("display", "none");
+        $('form table tbody tr td a').each(function() {
+            $(this).css("pointer-events", "none");
+        });
+      }
+      else if(myRegExGuy.test(thePageIAmOn)){
+        console.log('nah');
+        window.location.href = 'https://us.idexxneo.com/products/';
       }
     }
 });
